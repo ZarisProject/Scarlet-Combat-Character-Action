be06a2d4cfeeeab4203ba4214e12e8cbd8ac7986
K <Akarfire@gmail.com>
2026-09-07 17:51:47 +0300

    [DEV] Combo Reset Notify files

---
+++ Source/ScarletCombat_CharacterAction/Public/SC_ComboResetNotify.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "SC_ComboResetNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCARLETCOMBAT_CHARACTERACTION_API USC_ComboResetNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
