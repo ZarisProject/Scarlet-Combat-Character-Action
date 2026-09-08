9685c7a9b712c0b5378086919e12103bcd5a331d
K <Akarfire@gmail.com>
2026-09-08 16:05:20 +0300

    [DEV] Combo input notify implemented

--- Source/ScarletCombat_CharacterAction/Private/SC_ComboInputNotify.cpp
+++ Source/ScarletCombat_CharacterAction/Private/SC_ComboInputNotify.cpp
@@ -3,9 +3,47 @@
 
 #include "SC_ComboInputNotify.h"
 
+#include "SC_CharacterAction.h"
+#include "SC_CharacterActionInterface.h"
+
 // Serving input to the input notify
 // Returns the result of ADDITIONAL checks (if such are present)
 bool USC_ComboInputNotify::ServeInput(const FName& Input)
 {
-    return false;
+    return CheckAdditionalConditions(Cached_MeshComp, Cached_Animation, Cached_OwnerActor, Cached_CharacterAction, Input);
+}
+
+void USC_ComboInputNotify::NotifyBegin( USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, 
+                                        float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+    Duration = TotalDuration;
+    CurrentTime = 0.0f;
+
+    // Caching data
+    Cached_MeshComp = MeshComp;
+    Cached_Animation = Animation;
+    Cached_OwnerActor = MeshComp->GetOwner();
+
+    if (Cached_OwnerActor && Cached_OwnerActor->Implements<USC_CharacterActionInterface>())
+    {
+        USC_CharacterAction* CharacterAction = ISC_CharacterActionInterface::Execute_GetCharacterActionComponent(Cached_OwnerActor);
+        Cached_CharacterAction = CharacterAction;
+    }
+}
+
+void USC_ComboInputNotify::NotifyTick(  USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                        float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+    CurrentTime += FrameDeltaTime;
+
+    if (Cached_CharacterAction)
+    {
+        for (auto& Input : Inputs)
+            Cached_CharacterAction->RequestInput(Input, this, Priority);
+    }
+}
+
+void USC_ComboInputNotify::NotifyEnd(   USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, 
+                                        const FAnimNotifyEventReference& EventReference)
+{
 }
--- Source/ScarletCombat_CharacterAction/Public/SC_ComboInputNotify.h
+++ Source/ScarletCombat_CharacterAction/Public/SC_ComboInputNotify.h
@@ -32,9 +32,54 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
 	bool OverrideExistingKey = false;
 
+protected:
+
+	// Runtime context data
+	float Duration = 0.f;
+	float CurrentTime = 0.f;
+
+	// Cache
+	USkeletalMeshComponent* Cached_MeshComp;
+	UAnimSequenceBase* Cached_Animation;
+	AActor* Cached_OwnerActor;
+	class USC_CharacterAction* Cached_CharacterAction;
+
 public:
 
 	// Serving input to the input notify
 	// Returns the result of ADDITIONAL checks (if such are present)
 	bool ServeInput(const FName& Input);
+
+
+	virtual void NotifyBegin(	USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, 
+								float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+
+	virtual void NotifyTick(	USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+								float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+
+	virtual void NotifyEnd(		USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, 
+								const FAnimNotifyEventReference& EventReference) override;
+
+
+	// Returns notify's duration in seconds (works correctly after NotifyBegin)
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "ScarletCombat|CharacterAction|ComboInputNotify")
+	float GetDuration() const { return Duration; }
+
+	// Returns time since last NotifyBegin in seconds
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "ScarletCombat|CharacterAction|ComboInputNotify")
+	float GeCurrentTime() const { return CurrentTime; }
+
+
+	// Override this function to add additional conditions
+	UFUNCTION(BlueprintCallable, BlueprintPure, BlueprintNativeEvent, Category = "ScarletCombat|CharacterAction|ComboInputNotify")
+	bool CheckAdditionalConditions(	USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, 
+									AActor* OwnerActor, class USC_CharacterAction* CharacterAction,
+									const FName& Input) const;
+
+	virtual bool CheckAdditionalConditions_Implementation(
+		USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+		AActor* OwnerActor, class USC_CharacterAction* CharacterAction, const FName& Input) const
+	{
+		return true;
+	}
 };
